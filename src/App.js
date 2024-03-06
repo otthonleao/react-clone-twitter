df27ee5a4ea327d218335f7d9463e1b5f3f43194
Otthon Leão <otthonleao@hotmail.com>
2024-03-05 20:12:17 -0400

    chore: tentativa npm start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import styles from "./App.module.css";
-import Index from "./ui/pages";
+import Index from "./ui/pages/index.jsx";
 
 function App() {
 	return (
-		<div className={styles["app-container"]}>
+		<div className={styles['app-container']}>
 			<Index />
 		</div>
 	);
