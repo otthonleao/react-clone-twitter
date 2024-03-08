3be01d89fff32c719a197cc9032a72468aa1e15d
Otthon Leão <otthonleao@hotmail.com>
2024-03-08 13:42:15 -0400

    feat: add  component with

--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,7 +1,10 @@
 import { useIndex } from "../../data/hooks/pages/useIndex.page.js";
 import styles from "../styles/pages/index.module.css";
+import TextInput from "../../components/inputs/TextInputs/TextInput.jsx";
 
 export default function Index() {
 	useIndex();
-	return <div>Index</div>;
+	return (
+		<div><TextInput /></div>
+	);
 }
